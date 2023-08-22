6ba91927869dab9721e3383f4074063d0200a9c0
jay cooper <15jacooper@gmail.com>
2023-08-22 17:51:57 -0400

    push

--- index.js
+++ index.js
@@ -17,25 +17,25 @@ function createParagraph(){
     var wordSixteen = document.getElementById('wordSixteen').value;
     var wordSeventeen = document.getElementById('wordSeventeen').value;
     var wordEighteen = document.getElementById('wordEighteen').value;
-//     <p> Our dining (blank) used to be a war (blank).
-// I thought the battles about correct table (blank) would
+//     <p> Our dining (blank1) used to be a war (blank)2.
+// I thought the battles about correct table (blank3) would
 // never end. It was us kids versus Mom, and it seemed
-// like a fight that would last to the (blank) end.
-// But tonight Dad finally declared a/an (blank) truce, and we negotiated
-// a/an (blank) peace (blank). Mom promised
-// to no longer get (blank) upset and shoot us the dirty
-// (blank) and make (blank) remarks when
-// we do (blank) things she doesn't like.
+// like a fight that would last to the (blank4) end.
+// But tonight Dad finally declared a/an (blank5) truce, and we negotiated
+// a/an (blank6) peace (blank7). Mom promised
+// to no longer get (blank8) upset and shoot us the dirty
+// (blank9) and make (blank10) remarks when
+// we do (blank11) things she doesn't like.
 // We in turn agreed to:
 // <ol> <h2 class="list">
-// <li>Use napkins to wipe our (blank)and not our (blank).</li>
-// <li>Keep our (blank) off the table.</li>
-// <li>Not use our (blank) to pick up (blank) from our plates-except for sandwhiches and pieces of (blank).</li>
+// <li>Use napkins to wipe our (blank12)and not our (blank13).</li>
+// <li>Keep our (blank14) off the table.</li>
+// <li>Not use our (blank15) to pick up (blank16) from our plates-except for sandwhiches and pieces of (blank17).</li>
 // <li>Never talk with food in our (blank).</li>
 // </h2>
 // </ol>
 // </p>
-    var paragraph = '<p>' + 'Our dining ' + wordOne + ' used to be a war ' + wordTwo + ' . I thought the battles about correct table ' + wordThree + ' would never end. It was us kids versus Mom, and it seemed like a fight that would last to the ' + wordFour + ' end. But tonight Dad finally declared a/an ' + wordFive + ' truce, and we negotiated a/an ' + wordSix + ' peace ' + wordSeven + ' . Mom promised to no longer get ' + wordEight + ' upset and shoot us the dirty ' + wordNine + ' and make ' + wordTen + ' remarks when we do ' + wordEleven + " things she doesn't like. We in turn agreed to:" + '<li>' + "Use napkins to wipe our " + wordTwelve + ' and not our ' + wordThirteen + '.' + '</li>' + '<li>' + ' Keep our ' + wordFourteen + ' off the table. ' + '</li>' + '<li>' + 'Not use our ' +  wordFifteen + ' to pick up ' + wordSixteen + ' from our plates-except for sandwhiches and pieces of ' + wordSeventeen + '.' + '</li>' + '<li>'  +' Never talk with food in our' + wordEighteen + '.' + '</li>' + '</p>'
+    var paragraph = '<p>' + 'Our dining ' + wordOne + ' used to be a war ' + wordTwo + ' . I thought the battles about correct table ' + wordThree + ' would never end. It was us kids versus Mom, and it seemed like a fight that would last to the ' + wordFour + ' end. But tonight Dad finally declared a/an ' + wordFive + ' truce, and we negotiated a/an ' + wordSix + ' peace ' + wordSeven + ' . Mom promised to no longer get ' + wordEight + ' upset and shoot us the dirty ' + wordNine + ' and make ' + wordTen + ' remarks when we do ' + wordEleven + " things she doesn't like. We in turn agreed to:" + '<li>' + "Use napkins to wipe our " + wordTwelve + ' and not our ' + wordThirteen + '.' + '</li>' + '<li>' + ' Keep our ' + wordFourteen + ' off the table. ' + '</li>' + '<li>' + 'Not use our ' +  wordFifteen + ' to pick up ' + wordSixteen + ' from our plates-except for sandwhiches and pieces of ' + wordSeventeen + '.' + '</li>' + '<li>'  +' Never talk with food in our ' + wordEighteen + '.' + '</li>' + '</p>'
         
     document.getElementById('answer').innerHTML = paragraph
 }
